08b919eff701fa096bd5ae93402313c8e4ab4a9c
Dzmitry Zmitrachonak <Dzmitry_Zmitrachonak@epam.com>
2018-02-09 14:27:28 +0300

    Refactor code

--- aws.py
+++ aws.py
@@ -17,38 +17,24 @@ class AWS(BotPlugin):
         config = {
             'access_id': None,
             'secret_key': None,
-            'ami': None,
-            'keypair': None,
-            'subnet_id': None,
-            'route_table_id': None,
-            'volume_size': 1,
-            'instance_type': None,
-            'datacenter': None,
-            'puppet': False,
+            'region': None
         }
         return config
-
 
     def _connect(self):
         """ connection to aws """
         access_id = self.config['access_id']
         secret_key = self.config['secret_key']
-        datacenter = self.config['datacenter']
+        region = self.config['region']
 
-        cls = get_driver(datacenter)
-        driver = cls(access_id, secret_key)
+        cls = get_driver('ec2')
+        driver = cls(access_id, secret_key, region=region)
         return driver
 
     def _find_instance_by_name(self, name):
         driver = self._connect()
         for instance in driver.list_nodes():
             if instance.name == name:
-                return instance
-                
-    def _find_instance_by_id(self, id):
-        driver = self._connect()
-        for instance in driver.list_nodes():
-            if instance.id == id:
                 return instance
 
     def _basic_instance_details(self, name):
@@ -69,22 +55,23 @@ class AWS(BotPlugin):
 
         return details
 
-    @botcmd(split_args_with=' ')
+    @botcmd
     def aws_info(self, msg, args):
         ''' get details of a virtual machine
             options: name
+            example:
+            !aws info vm1
         '''
         vmname = args.pop(0)
         details = self._basic_instance_details(vmname)
-        self.send(msg.getFrom(), '{0}: {1}'.format(vmname, details), message_type=msg.getType())
+        self.send(msg.frm, '{0}: {1}'.format(vmname, details))
 
     @botcmd
     def aws_reboot(self, msg, args):
         ''' reboot a virtual machine
-            options:
-                vm (name): name of virtual machine
+            options: name
             example:
-            !aws vm_reboot log1
+            !aws reboot vm1
         '''
         vm = self._find_instance_by_name(args)
         result = vm.reboot()
@@ -94,16 +81,15 @@ class AWS(BotPlugin):
         else:
             response = 'Unable to complete request.'
 
-        self.send(msg.getFrom(), '{0}: {1}'.format(vm.name, response), message_type=msg.getType())
+        self.send(msg.frm, '{0}: {1}'.format(vm.name, response))
 
 
     @botcmd
     def aws_terminate(self, msg, args):
         ''' terminate/destroy a virtual machine
-            options:
-                vm (name): name of instance
+            options: name
             example:
-            !aws vm_terminate log1
+            !aws terminate vm1
         '''
         vm = self._find_instance_by_name(args)
         result = vm.destroy()
@@ -113,86 +99,4 @@ class AWS(BotPlugin):
         else:
             response = 'Unable to complete request.'
 
-        self.send(msg.getFrom(), '{0}: {1}'.format(vm.name, response), message_type=msg.getType())
-
-    @botcmd(split_args_with=' ')
-    def aws_create(self, msg, args):
-        ''' create a virtual machine from ami template
-            options:
-                ami (str): template ami to use
-                size (int): disk size of instance in GBs
-                tags (str): key=val tags
-                subnet_id (str): vpc subnet
-                route_table_id (str): vpc subnet's routing table
-                keypair (str): key pair to use
-                instance_type (str): ami instance type
-                puppet (bool): run puppet after provisioning
-            example:
-            !aws create --ami=i-12321 --size=20 --tags="key1=val1,key2=val2" --keypair=my-key --instance_type=t2.medium --puppet app-server1
-        '''
-        parser = OptionParser()
-        parser.add_option("--ami", dest="ami", default=self.config['ami'])
-        parser.add_option("--size", dest="size", type='int', default=15)
-        parser.add_option("--subnet_id", dest="subnet_id", default=self.config['subnet_id'])
-        parser.add_option("--route_table_id", dest="route_table_id", default=self.config['route_table_id'])
-        parser.add_option("--instance_type", dest="instance_type", default=self.config['instance_type'])
-        parser.add_option("--tags", dest="tags")
-        parser.add_option("--keypair", dest="keypair", default=self.config['keypair'])
-        parser.add_option("--puppet", action="store_false",
-                          dest="puppet", default=self.config['puppet'])
-
-        (t_options, t_args) = parser.parse_args(args)
-        options = vars(t_options)
-
-        vmname = t_args.pop(0)
-
-        # setting up requirements
-        network = EC2SubnetAssociation(id=options['subnet_id'],
-                                       route_table_id=options['route_table_id'],
-                                       subnet_id=options['subnet_id'],
-                                       main=True)
-
-        block_dev_mappings = [{'VirtualName': None,
-                               'Ebs': {
-                                    'VolumeSize': options['size'],
-                                    'VolumeType': 'standard',
-                                    'DeleteOnTermination': 'true'},
-                                    'DeviceName': '/dev/sda'}]
-
-        base_tags = {
-            'Name': vmname,
-            'team': 'systems',
-        }
-
-
-        if options['tags'] is not None:
-            for t_tags in options['tags'].split(','):
-                base_tags.update(dict([keys.split('=')]))
-
-        driver = self._connect()
-
-        # Setting up ami + instance type
-        sizes = driver.list_sizes()
-        size = [s for s in sizes if s.id == options['instance_type']][0]
-        image = NodeImage(id=options['ami'], name=None, driver=driver)
-
-        # using key-pair and group
-        node = driver.create_node(name=vmname, image=image, size=size,
-                                  ex_keyname=options['keypair'],
-                                  #ex_securitygroup=SECURITY_GROUP_NAMES, # issue for now
-                                  ex_subnet=network,
-                                  ex_blockdevicemappings=block_dev_mappings,
-                                  ex_metadata=base_tags)
-
-        self.send(msg.getFrom(), '{0}: [1/3] Creating instance'.format(vmname), message_type=msg.getType())
-        # todo: actually query state of instance
-        #time.sleep(30)
-        self.send(msg.getFrom(), '{0}: [2/3] Running post setup'.format(vmname), message_type=msg.getType())
-
-        if options['puppet']:
-            # ready for puppet... let's go!
-            self.send(msg.getFrom(), '{0}: Running puppet [disabled]'.format(vmname), message_type=msg.getType())
-
-        self.send(msg.getFrom(), '{0}: [3/3] Request completed'.format(vmname), message_type=msg.getType())
-        self.send(msg.getFrom(), '{0}: {1}'.format(vmname, self._basic_instance_details(vmname)), message_type=msg.getType())
-
+        self.send(msg.frm, '{0}: {1}'.format(vm.name, response))
